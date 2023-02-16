950c4e26c79874bbba2072460c3c0fe83fbdf0a1
AlessandroGia <alexgiacento@gmail.com>
2023-02-16 19:49:42 +0100

    fix bugs

--- contentscanner/main.py
+++ contentscanner/main.py
@@ -1,37 +1,40 @@
 from contentscanner.ContentScanner import ContentScanner
 from .exceptions.exceptions import ExceptionUrl, ExceptionGetWordlist
 from argparse import ArgumentParser
+from dotenv import load_dotenv
 from asyncio import run
 from sys import exit
 
+import os
+
 
 async def __run(args):
+    if not args.path and os.getenv('WORDLIST'):
+        load_dotenv()
+        path = os.getenv('WORDLIST')
+    else:
+        path = ''
     try:
-        content_scanner = ContentScanner(args.url, r'' + args.path)
-    except Exception as e:
-        exit('Errore: {}'.format(e.with_traceback(None)))
+        content_scanner = ContentScanner(args.url, r'' + path)
+    except ExceptionUrl:
+        exit("Problem with given url.")
     else:
         try:
             await content_scanner.scan()
         except ExceptionGetWordlist:
             exit("Something went wrong while opening wordlist file.")
-        except ExceptionUrl:
-            exit("Problem with given url.")
 
 
 def main():
     parser = ArgumentParser(description='Web Content Scanner.')
-    parser.add_argument("path", help="wordlist's path")
     parser.add_argument(
-            '--path', default=False, action="store_true",
-            required=True, help="wordlist's path"
+            '--url', required=True, help="website's url"
         )
-    parser.add_argument("url", help="website's url")
     parser.add_argument(
-            '--url', default=False, action="store_true",
-            required=True, help="website's url"
+            '--path', required=False, help="wordlist's path"
         )
-    run(__run(parser.parse_args()))
+    args = parser.parse_args()
+    run(__run(args))
 
 
 if __name__ == '__main__':
